d5bbded08b5249205f38b7a60bd3f64c75586d77
kiryong159 <kiryong159@naver.com>
2023-03-13 20:16:19 +0900

    한번더

--- src/App.js
+++ src/App.js
@@ -3,45 +3,46 @@ import { useState, useEffect } from "react";
 //import styles from "./App.module.css";
 
 function App() {
-  const [loading, setloading] = useState(true);
+  const [loding, setloding] = useState(true);
   const [coins, setCoins] = useState([]);
   const [money, setMoney] = useState(0);
-  const [coinPrice, setcoinPrice] = useState(0);
+  const [selectCoin, setSelectCoin] = useState(0);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
-        setloading(false);
+        setloding(false);
       });
   }, []);
 
-  const moneyChange = (event) => {
-    setMoney(event.target.value);
+  const coinPrice = (event) => {
+    setSelectCoin(event.target.value);
   };
-  const selectcoin = (event) => {
-    setcoinPrice(event.target.value.split(":")[1].split("원")[0]);
+  const moneyset = (e) => {
+    setMoney(e.target.value);
   };
   return (
     <div>
-      <h1> The coins ({coins.length})</h1>
-      <h3>{loading ? "LOADDING" : null}</h3>
-      <h3>가지고있는돈(원)</h3>
-      <input value={money} type="number" onChange={moneyChange} />
-
-      <div>
-        <h3>가지고싶은 코인</h3>
-        <select onChange={selectcoin}>
-          <option>코인을고르세오</option>
-          {coins.map((item, index) => (
-            <option key={index}>
-              {item.name}({item.symbol}) :{" "}
-              {Math.round(item.quotes.USD.price * 13060) / 10} 원
-            </option>
-          ))}
-        </select>
-      </div>
-      <h3>{Math.round((money / coinPrice) * 100) / 100}개 구매가능</h3>
+      <h1>The Coins ({coins.length})</h1>
+      {loding ? (
+        <h1>로딩중</h1>
+      ) : (
+        <>
+          <input value={money} onChange={moneyset} type="number" />
+          <select onChange={coinPrice}>
+            {coins.map((item) => (
+              <option
+                key={item.id}
+                value={Math.round(item.quotes.USD.price * 13060) / 10}
+              >
+                {item.name} : {Math.round(item.quotes.USD.price * 13060) / 10}
+              </option>
+            ))}
+          </select>
+          {Math.round((money / selectCoin) * 10) / 10}
+        </>
+      )}
     </div>
   );
 }
