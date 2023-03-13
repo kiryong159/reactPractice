a24b0860d5b4683d1116a332b9ca2f47be81b917
kiryong159 <kiryong159@naver.com>
2023-03-13 19:40:43 +0900

    7.2 챌린저

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import Button from "./button";
-import styles from "./App.module.css";
+// import Button from "./button";
+import { useState, useEffect } from "react";
+//import styles from "./App.module.css";
 
 function App() {
+  const [loading, setloading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [coinPrice, setcoinPrice] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setloading(false);
+      });
+  }, []);
+
+  const moneyChange = (event) => {
+    setMoney(event.target.value);
+  };
+  const selectcoin = (event) => {
+    setcoinPrice(event.target.value.split(":")[1].split("원")[0]);
+  };
   return (
     <div>
-      <h1 className={styles.title}>welcome</h1>
-      <Button text="hi" />
+      <h1> The coins ({coins.length})</h1>
+      <h3>{loading ? "LOADDING" : null}</h3>
+      <h3>가지고있는돈(원)</h3>
+      <input value={money} type="number" onChange={moneyChange} />
+
+      <div>
+        <h3>가지고싶은 코인</h3>
+        <select onChange={selectcoin}>
+          <option>코인을고르세오</option>
+          {coins.map((item, index) => (
+            <option key={index}>
+              {item.name}({item.symbol}) :{" "}
+              {Math.round(item.quotes.USD.price * 13060) / 10} 원
+            </option>
+          ))}
+        </select>
+      </div>
+      <h3>{Math.round((money / coinPrice) * 100) / 100}개 구매가능</h3>
     </div>
   );
 }
--- src/button.js
+++ src/button.js
@@ -1,7 +1,29 @@
 import PropTypes from "prop-types";
 import styles from "./button.module.css";
+import styles2 from "./App.module.css";
+import { useState, useEffect } from "react";
+
 const Button = ({ text }) => {
-  return <button className={styles.Btn}>{text}</button>;
+  const [show, setShow] = useState(false);
+  const onClick = () => {
+    setShow((current) => !current);
+  };
+  const Hello = () => {
+    useEffect(() => {
+      console.log("ㅎㅇ");
+      return () => console.log("ㅂㅇ");
+    }, []);
+    return <h1>안녕</h1>;
+  };
+
+  return (
+    <div>
+      {show ? <Hello /> : null}
+      <button onClick={onClick} className={styles.Btn}>
+        {show ? "보이는상태" : "숨은상태"}
+      </button>
+    </div>
+  );
 };
 
 Button.propTypes = {
